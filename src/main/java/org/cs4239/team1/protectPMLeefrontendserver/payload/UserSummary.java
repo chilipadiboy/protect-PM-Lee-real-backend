ea6bce190bba524b9ac07e5c38e5265d1fa7e725
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-09-28 14:43:37 +0800

    Teach RestController to return current user

---
+++ src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/UserSummary.java
@@ -0,0 +1,11 @@
+package org.cs4239.team1.protectPMLeefrontendserver.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserSummary {
+    private String nric;
+    private String name;
+}
